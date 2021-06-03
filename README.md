348912c1137a7166777c8fefc39c0a5a6a5dc41b
Arsalan Noorafkan <arsalan@atlys.ca>
2021-06-03 11:17:37 -0400

    Final Submission

--- README.md
+++ README.md
@@ -1,2 +1,160 @@
-# Sparkify-Data-Lake-Project
- 
+
+
+<br>
+
+# Udacity Data Engineering Nanodegree | Data Lake Project
+
+<br>
+
+### *Arsalan Noorafkan*
+
+**2021-06-01**
+<br>
+<br>
+
+# **Overview**
+## Background
+
+A music streaming startup, Sparkify, has grown their user base and song database even more and want to move their data warehouse to a data lake. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+
+As their data engineer, you are tasked with building an ETL pipeline that extracts their data from S3, processes them using Spark, and loads the data back into S3 as a set of dimensional tables. This will allow their analytics team to continue finding insights in what songs their users are listening to.
+
+You'll be able to test your database and ETL pipeline by running queries given to you by the analytics team from Sparkify and compare your results with their expected results.
+
+In this project, you'll apply what you've learned on Spark and data lakes to build an ETL pipeline for a data lake hosted on S3. To complete the project, you will need to load data from S3, process the data into analytics tables using Spark, and load them back into S3. You'll deploy this Spark process on a cluster using AWS.
+
+##### ***These facts are for a fictional company that does not exist in real life***
+
+<br>
+
+## Purpose
+
+As a startup, Sparkify's analytics team needs a fast and accurate process for answering questions that support business decisions. However, since the data collected from users is rapidly increasing as they grow the business, hence, the raw data needs to be cleaned, transformed, and prepared as a star schema optimized for queries on song play analysis. The ETL process helps the analytics team work with a relational database instead of raw JSON files. 
+
+<br>
+
+---
+
+<br>
+
+# **Schema**
+
+The database will be structured as a star schema that comprises of a fact table with multiple dimension tables. This schema creates denormalized tables that help simplify queries and enable faster aggregations even when using lots of group by, joins, having, and where clauses. It must be noted that a star schema may be less flexible when working on ad-hoc queries and many-to-many relationships are not supported. 
+
+
+### **Fact Table**
+* songplays - records in event data associated with song plays i.e. records with page NextSong
+    <br> `songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent`
+
+### **Dimension Tables**
+* users - users in the app
+    <br> `user_id, first_name, last_name, gender, level`
+* songs - songs in music database
+    <br> `song_id, title, artist_id, year, duration`
+* artists - artists in music database
+    <br> `artist_id, name, location, lattitude, longitude`
+* time - timestamps of records in songplays broken down into specific units
+    <br> `start_time, hour, day, week, month, year, weekday`
+
+
+
+### **Schema Diagram** 
+![Start Schema](assets/schema.png)
+
+
+
+
+
+
+
+
+
+<br>
+<br>
+<br>
+
+
+# **Instructions**
+## Specifications
+The ETL process comprises of the following technical specifications:
+- Raw data is provided as JSON files stored in S3
+
+    * SONG_DATA = 's3://udacity-dend/song_data'
+    <br> 
+        * song_data = s3a://udacity-dend/song_data/A/A/A/*.json
+        * Sample data:
+    <br> 
+        ![Song Data](assets/pic2.jpg)
+    <br> 
+    
+    * LOG_DATA = 's3://udacity-dend/log-data'
+    <br>
+        * log_data = s3a://udacity-dend/log_data/2018/11/*.json
+        * Sample data:
+    <br> 
+        ![Log Data](assets/log-data.png)
+    <br> 
+    
+- ETL script is written in Python
+    - Python libraries include PySpark (SQL Functions) and ConfigParser, DateTime, and OS.
+
+- AWS resources: S3 bucket (for output parquet files) cluster and IAM role
+
+
+
+<br>
+
+## Project Structure
+- dl.cfg: AWS Access Key ID and Secret Access Key
+- etl.py: load data from S3 into staging tables stored in S3
+
+<br> 
+
+## ETL Steps
+The ETL process comprises of the following steps:
+
+1) create_spark_session
+    - initialize spark session builder
+    - configure session parameters
+    - create or get session if exists
+2) process_song_data
+    - loads input JSON files from S3: song_data 
+    - extracts data columns
+    - write songs and artists parquet files to S3
+3) process_log_data
+    - loads input JSON files from S3: log_data 
+    - extracts data columns - dimension tables
+    - extracts data columns - fact table
+    - write user and time parquet files to S3
+
+
+<br>
+
+
+## Testing Script
+Follow the steps below to test the ETL process using sample JSON data files.
+
+The ETL process comprises of the following steps:
+
+1) Install Python libraries
+
+2) Create AWS resources
+    - S3 bucket us-east-1
+    - Enter AWS config and authentication information in the cl.cfg file
+
+2) Open a terminal window and cd to the folder that contains the etl.py file
+    > cd c:/usr/documents/Project1
+
+3) Run the ETL process and stage the data for analysis by executing the following from the command line:
+    >  `python etl.py`
+
+
+<br>
+REMINDER: Do not include your AWS access keys in your code when sharing this project!
+<br>
+
+---
+
+
+<br>
+
--- etl.py
+++ etl.py
@@ -1,94 +1,227 @@
 import configparser
-from datetime import datetime
+from datetime import datetime, date
 import os
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format, dayofweek, monotonically_increasing_id
+from pyspark.sql.types import TimestampType, DateType
 
 
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID']= config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']= config['AWS']['AWS_SECRET_ACCESS_KEY']
 
 
+
+# Starts a new Spark Session
 def create_spark_session():
+    print('\n\Spark Session Starting...\n\n')
+    '''
+    Function: 
+        1) initialize spark session builder
+        2) configure session parameters
+        3) create or get session if exists
+
+    Parameters:
+        None
+    
+    Returns:
+        spark session curser
+    '''
     spark = SparkSession \
         .builder \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.0') \
         .getOrCreate()
     return spark
 
 
+# Process song data files
 def process_song_data(spark, input_data, output_data):
+    print('\n\nProcessing Song Data...\n\n')
+    '''
+    Function: 
+        1) loads input JSON files from S3: song_data 
+        2) extracts data columns
+        3) write songs and artists parquet files to S3
+
+    Parameters:
+        spark: the cursor
+        input_path: path to the S3 bucket with song data
+        output_path: path to S3 bucket where parquet files are stored
+    
+    Returns:
+        None
+    '''
+
+    
+
+    ## STEP 1 - LOAD INPUT DATA
+
     # get filepath to song data file
-    song_data = 
-    
+    # song_data = input_data+'/song_data/*/*/*/*.json'
+    song_data = input_data+'/song_data/A/A/A/*.json'
+
     # read song data file
-    df = 
+    df = spark.read.json(song_data)
+    print('Completed: input file song_data loaded from S3')
 
+
+
+    ## STEP 2 - EXTRACT DIMENSIONAL COLUMNS
+
+    # songs - songs in music database
+        # song_id, title, artist_id, year, duration
+    
     # extract columns to create songs table
-    songs_table = 
-    
-    # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table = df.select(
+        'song_id', 'title', 'artist_id', 'year', 'duration'
+        ).dropDuplicates()
+    print('Completed: songs_table columns extracted')
 
+    # artists - artists in music database
+        # artist_id, name, location, lattitude, longitude
+    
     # extract columns to create artists table
-    artists_table = 
-    
+    artists_table = df.select(
+        'artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude'
+        ).dropDuplicates()
+    print('Completed: artists_table columns extracted')
+
+
+
+    ## STEP 3 - WRITE PARQUET FILES
+
+    # write songs table to parquet files partitioned by year and artist
+    songs_table.write.parquet(output_data+'/songs_table',
+                              mode='overwrite',
+                              partitionBy=['year', 'artist_id'])
+    print('Completed: songs_table written to S3')
+
     # write artists table to parquet files
-    artists_table
+    artists_table.write.parquet(output_data+'/artists_table',
+                              mode='overwrite')
+    print('Completed: artists_table written to S3')
 
 
+    print('\n\nCompleted: song data processed!\n\n')
+
+
+
+# Process log data files
 def process_log_data(spark, input_data, output_data):
-    # get filepath to log data file
-    log_data =
+    print('\n\nProcessing Log Data...\n\n')
+    '''
+    Function: 
+        1) loads input JSON files from S3: log_data 
+        2) extracts data columns - dimension tables
+        3) extracts data columns - fact table
+        4) write user and time parquet files to S3
+    Parameters:
+        spark: the cursor
+        input_path: path to the S3 bucket with song data
+        output_path: path to S3 bucket where parquet files are stored
+    Returns:
+        None
+    '''
 
-    # read log data file
-    df = 
-    
-    # filter by actions for song plays
-    df = 
+    ## STEP 1 - LOAD INPUT DATA
 
-    # extract columns for users table    
-    artists_table = 
-    
-    # write users table to parquet files
-    artists_table
-
-    # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
-    
-    # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
-    
-    # extract columns to create time table
-    time_table = 
-    
-    # write time table to parquet files partitioned by year and month
-    time_table
+    # get filepath to song data file
+    # log_data = input_data+'/log_data/*/*/*/*.json'
+    log_data = input_data+'/log_data/2018/11/*.json'
 
     # read in song data to use for songplays table
-    song_df = 
+    # song_data = input_data+'/song_data/*/*/*/*.json'
+    song_data = input_data+'/song_data/A/A/A/*.json'
+    dfSong = spark.read.json(song_data)
+    
+    # read song data file
+    df = spark.read.json(log_data)
+    # filter by actions for song plays
+    df = df.filter(col('page') == 'NextSong')
+    print('Completed: input files log_data and song data loaded from S3')
+    
+
+
+    ## STEP 3 - EXTRACT DIMENSIONAL COLUMNS
+
+    # users - users in the app
+        # userId, first_name, last_name, gender, level
+    users_table = df.select(
+        'ts', 'userId', 'firstName', 'lastName', 'gender', 'level'
+    ).dropDuplicates()
+    print('Completed: users_table columns extracted')
+    
+    # create timestamp column from original timestamp column
+    get_timestamp = udf(lambda epoch: datetime.fromtimestamp(epoch / 1000.0), TimestampType())
+    df = df.withColumn('start_time', get_timestamp('ts'))
+
+    # create datetime column from original timestamp column
+    get_datetime = udf(lambda epoch: date.fromtimestamp(epoch / 1000.0), DateType())
+    df = df.withColumn('date', get_datetime('ts'))
+
+    # extract columns to create time table
+    df = df.withColumn('date_year', year('date'))
+    df = df.withColumn('date_month', month('date'))
+    
+    time_table = df.select('start_time', 
+                           hour('date').alias('date_hour'), 
+                           dayofmonth('date').alias('date_day'), 
+                           weekofyear('date').alias('date_week'), 
+                           month('date').alias('date_month'),
+                           year('date').alias('date_year'),
+                           dayofweek('date').alias('date_weekday')
+                        ).distinct()
+    
+    
+
+    ## STEP 2 - EXTRACT FACT COLUMNS
 
     # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+    cond = [dfSong['title'] == df['song'], dfSong['artist_name'] == df['artist']]
+    df = df.join(dfSong, cond)
+    songplays_table = df[
+        'start_time', 'userId', 'level', 'song_id', 'artist_id', 'sessionId', 'location', 'userAgent', 'date_year', 'date_month'
+        ]
+    songplays_table.select(monotonically_increasing_id().alias('songplay_id')).collect()
+
+
+
+    ## STEP 4 - WRITE PARQUET FILES
+
+    # write users table to parquet files
+    users_table.write.parquet(output_data+'/user_table', 'overwrite')
+    print('Completed: users_table written to S3')
+
+    # write time table to parquet files partitioned by year and month
+    time_table.write.parquet(output_data+'/time_table',
+                             mode='overwrite',
+                             partitionBy=['date_year', 'date_month'])
+    print('Completed: time_table written to S3')
 
     # write songplays table to parquet files partitioned by year and month
-    songplays_table
+    songplays_table.write.partitionBy('date_year', 'date_month').parquet(os.path.join(output_data, 'songplays.parquet'), 'overwrite')
+
+    print('\n\nCompleted: log data processed!\n\n')
+
 
 
 def main():
+    print('\n\n\nSparkify Data Lake ETL Process Started...\n\n\n')
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = ""
     
+    input_data = 's3a://udacity-dend/'
+    output_data = 's3a://sparkifydatalake231/'
+     
+
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
 
+    print('\n\n\nSparkify Data Lake ETL Process Complete!\n\n\n')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
